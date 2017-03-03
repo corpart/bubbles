d9c030b4f25e7814ed6a6b1c87f9d789fda4b751
cheng <gnuchex@gmail.com>
2017-03-02 19:39:02 -0800

    add label

---
+++ js/material.js
@@ -0,0 +1,52 @@
+  function getSpriteMaterial( r, groupID ){
+        return  new THREE.SpriteCanvasMaterial( {
+            color: colors[groupID],
+            program: function ( context ) {
+                context.beginPath();
+                context.arc( 0, 0, r, 0, PI2, true );
+                context.fill();
+            }
+        } );
+
+    }
+
+
+
+    //for the triangles
+    function getEdgeMaterial (answerID){
+        return new THREE.MeshBasicMaterial( {
+            color: colors[answerID],
+            // color: 0X666666,
+            opacity: 0.3,
+            wireframe: true
+             } )
+    }
+
+    function getFaceMaterial (answerID){
+        return new THREE.MeshBasicMaterial( { color: colors[answerID], opacity:0.2,
+            side: THREE.DoubleSide ,
+            transparent: true,
+            blending: THREE.AdditiveBlending } );
+
+
+    }
+
+    function generateSprite() {
+
+        var canvas = document.createElement( 'canvas' );
+        canvas.width = 16;
+        canvas.height = 16;
+
+        var context = canvas.getContext( '2d' );
+        var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
+        gradient.addColorStop( 0, 'rgba(255,255,255,1)' );
+        gradient.addColorStop( 0.2, 'rgba(100,255,255,1)' );
+        gradient.addColorStop( 0.4, 'rgba(0,0,21,1)' );
+        gradient.addColorStop( 1, 'rgba(0,0,0,1)' );
+
+        context.fillStyle = gradient;
+        context.fillRect( 0, 0, canvas.width, canvas.height );
+
+        return canvas;
+
+    }
